bcfc6f9109f1c586509028c219bdfac70482d609
MitjaCH <kurathmitja@gmail.com>
2024-02-15 18:40:15 +0100

    [FIX] Fixed all RunTimeErrors in setup.js

--- pages/setup.js
+++ pages/setup.js
@@ -1,8 +1,7 @@
-// SetupPage.js
 import styles from "../styles/setup.module.css";
 import Title from "../components/title";
 import TableOfContents from "../components/tableOfContents";
-import CommandContainer from "../components/commandContainer"; // Import CommandContainer component
+import CommandContainer from "../components/commandContainer";
 import Link from "next/link";
 
 export default function SetupPage() {
@@ -22,16 +21,23 @@ export default function SetupPage() {
             <p>
               First you have to Download Node.js from{" "}
               <span>
-                <Link href="https://nodejs.org/en">Node.js</Link>
+                <Link href="https://nodejs.org/en" target="_blank">[Node.js]</Link>
               </span>
             </p>
             <img src="/download-node-js-image.png" alt="Node.js Downloading Image" />
             <p>
               After you've done that run the Node.js Setup. And Open the Terminal
-              Windows or VS Code and Type: <CommandContainer text="node --version" showCopyButton={true}/>
+              Windows or VS Code and Type: 
+            </p>
+
+              <CommandContainer text="node --version" showCopyButton={true}/>
+            <p>
               to check if its Installed.
               The Output should look something like this:
+            </p>
               <CommandContainer text="v20.11.1 [depends on Version]"></CommandContainer>
+            <p>
+              Once you've checked that you Installed it correctly open Visual Studio Code. If you haven't Installed it yet Install it <Link href="https://code.visualstudio.com" target="_blank">[Here]</Link>
             </p>
       </div>
     </div>
