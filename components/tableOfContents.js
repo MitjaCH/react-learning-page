f2bc65389b49fa929118fc557d2982cf90416391
MitjaCH <mitjakur3b@gmail.com>
2024-02-15 17:16:17 +0100

    [UPDATE] Finalized tableOfContents.js & title.js

--- components/tableOfContents.js
+++ components/tableOfContents.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/tableOfContents.module.css'
+import styles from '../styles/tableOfContents.module.csss'
 
 function TableOfContents({ headings }) {
   return (
